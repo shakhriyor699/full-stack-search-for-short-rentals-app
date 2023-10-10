c861bba876d2b2986110de38c9bf8ce7f7807985
SHAHRIYOR S. IKROMITDINOV <s_ikromitdinov@uzinfocom.local>
2023-10-10 16:56:19 +0500

    finish listing creation step 1 & step 2

---
+++ components/inputs/CategoryInput.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { FC } from "react"
+import { IconType } from "react-icons"
+
+
+interface CategoryInputProps {
+  onClick: (value: string) => void
+  selected?: boolean
+  label: string
+  icon: IconType
+}
+
+const CategoryInput: FC<CategoryInputProps> = ({
+  onClick,
+  selected,
+  label,
+  icon: Icon
+}) => {
+  return (
+    <div
+      onClick={() => onClick(label)}
+      className={`
+        rounded-xl
+        p-4
+        border-2
+        flex
+        flex-col
+        gap-3
+        hover:border-black
+        transition
+        cursor-pointer
+        ${selected ? 'border-black' : 'border-neutral-200'}
+      `}
+    >
+      <Icon size={30} />
+      <div className="font-semibold">
+        {label}
+      </div>
+    </div>
+  )
+}
+
+export default CategoryInput
